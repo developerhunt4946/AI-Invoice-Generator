9b34f71a0f4de2339b0c2c1fd7b392e09457fa71
AyushGupta4946 <aagm0404@gmail.com>
2025-11-06 16:19:24 +0530

    implimented hero section

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Header from '../../components/landing/Header'
+import Hero from '../../components/landing/Hero'
+import Features from '../../components/landing/Features'
 
 const LandingPage = () => {
     return (
         <div className='bg-white text-grey-600'>
             <Header />
+            <main className='mb-[100vh]'>
+                <Hero />
+                <Features />
+            </main>
         </div>
     )
 }
