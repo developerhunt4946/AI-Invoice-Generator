9de185e3bdbfee1027dad4ee4733ca4cc29ca9ef
AyushGupta4946 <aagm0404@gmail.com>
2025-11-10 14:26:47 +0530

    implimented features sections

---
+++ src/utils/data.js
@@ -0,0 +1,28 @@
+import { BarChart2, FileText, Mail, Sparkles } from "lucide-react";
+
+export const FEATURES = [
+  {
+    icon: Sparkles,
+    title: "AI-Invoice Creation",
+    description:
+      "Experience blazing-fast load times and smooth interactions powered by optimized algorithms and efficient resource handling. Designed to keep your workflow running without delays, even under heavy usage.",
+  },
+  {
+    icon: BarChart2,
+    title: "AI-Powered Dashboard",
+    description:
+      "Your data is protected with advanced, industry-standard encryption and continuous security monitoring. We prioritize privacy and safeguard your information against unauthorized access or misuse.",
+  },
+  {
+    icon: Mail,
+    title: "Smart Reminders",
+    description:
+      "Easily tailor features, layouts, and settings to match your preferences. From modular components to adjustable configurations, the platform adapts to your unique requirements.",
+  },
+  {
+    icon: FileText,
+    title: "Easy Invoice Management",
+    description:
+      "Work from anywhere with seamless cloud connectivity and 24/7 server uptime. Designed for distributed teams, ensuring your content is always within reach—no matter your location.",
+  },
+];
